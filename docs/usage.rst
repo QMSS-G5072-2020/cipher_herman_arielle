1935120a034971b045c5f64a3bff3e24ca7477aa
Arielle Herman <aeh2196@columbia.edu>
2020-11-11 21:10:41 -0500

    first commit initial package setup

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use cipher_aeh2196 in a project::
+
+    import cipher_aeh2196
---
+++ tests/test_cipher_aeh2196.py
@@ -0,0 +1,5 @@
+from cipher_aeh2196 import __version__
+from cipher_aeh2196 import cipher_aeh2196
+
+def test_version():
+    assert __version__ == '0.1.0'
